b8d7203f33a425df2e687219d982c85c46e0a08e
mohdarshad1 <mdarshad181@gmail.com>
2021-04-27 19:21:07 +0530

    [Mohd Arshad] : Add Email Validation

--- lib/src/main/java/UserRegistration_JUnitTesting/UserRegistration.java
+++ lib/src/main/java/UserRegistration_JUnitTesting/UserRegistration.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
 	private static final String NAME_PATTERN = "^[A-Z][a-z]{2,}$";
+	private static final String EMAIL_ADDRESS_PATTERN = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$";
 	public boolean validateFirstName(String fname)
 	{
 		Pattern pattern = Pattern.compile(NAME_PATTERN);
@@ -14,5 +15,10 @@ public class UserRegistration {
 	{
 		Pattern pattern = Pattern.compile(NAME_PATTERN);
 		return pattern.matcher(lname).matches();
+	}
+	public boolean validateEmailAddress(String email)
+	{
+		Pattern pattern = Pattern.compile(EMAIL_ADDRESS_PATTERN);
+		return pattern.matcher(email).matches();
 	}
 }
